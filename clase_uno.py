3c34c140e88aeda8266ce67655314984eab93920
Andres Diaz <adiaz14@live.com>
2021-08-30 20:58:19 -0500

    Variables y operaciones en python

--- clase_uno.py
+++ clase_uno.py
@@ -4,4 +4,113 @@ Created on Mon Aug 30 20:19:26 2021
 
 @author: USER
 """
+import math
+
+print('Hello world')
+
+# Variables
+# No es tipado, el tipo de dato se acomoda al valor
+# asignado a la variable
+a = 3
+print(type(a))
+a = "Hola"
+print(type(a))
+a = 3.5
+print(type(a))
+a = True
+print(type(a))
+
+# pep8, es una buena practica en programacion py
+# estandar o guia de estilo de py, le da robustes
+# como escribir funciones, variables, vector, max de caracteres
+
+# Operaciones
+
+# Suma
+
+a = 5
+b = 2
+c = a + b
+print(c)
+
+# Resta
+
+a = 5
+b = 2
+c = a - b
+print(c)
+
+# Multiplicacion
+
+a = 5
+b = 2
+c = a * b
+print(c)
+
+# Division
+
+# Normal
+a = 5
+b = 2
+c = a / b
+print(c)
+
+# Normal tomando la parte entera
+
+a = 5
+b = 2
+c = a // b
+print(c)
+
+# Potencia
+
+a = 5
+b = 2
+c = a ** b
+print(c)
+
+# Raiz
+
+a = 27
+c = a ** (1/3)
+print(c)
+
+# import math
+raiz = math.sqrt(25)
+print(raiz)
+
+"""
+Esto
+es un comentario de 
+parrafo
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
